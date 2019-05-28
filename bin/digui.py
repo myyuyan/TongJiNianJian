af0bcef520418fd7180a754e9a99960f65883610
myyuyan <510294009@qq.com>
2019-05-29 01:06:37 +0800

    me

--- bin/digui.py
+++ bin/digui.py
@@ -24,8 +24,8 @@ def init_url(data):
                 'wds': json.dumps([]),
                 'dfwds': json.dumps([{"wdcode": "zb", "valuecode": page_json['id']}])
             }
-            page_data = requests.get(url=data_url,params=kw,headers=headers)
-            page_data_json = page_data.json()
+            page_data_k = requests.get(url=data_url,params=kw,headers=headers)
+            page_data_json = page_data_k.json()
             num_to_string = {}
             for page_data_ye in page_data_json['returndata']['wdnodes']:
                 if page_data_ye['wdcode']=='zb':
