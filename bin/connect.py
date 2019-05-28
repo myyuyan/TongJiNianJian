9b5c7436d11c50c00df924702f5f8edd5a49490f
myyuyan <510294009@qq.com>
2019-05-29 00:09:31 +0800

    me

--- bin/connect.py
+++ bin/connect.py
@@ -2,3 +2,5 @@ import pymongo
 from conf.config import *
 def connect():
     client = pymongo.MongoClient(host=MONGODB_HOST,port=MONGODB_PORT)
+    db = client[DB]
+    collection = db[COLLECTION]
--- conf/config.py
+++ conf/config.py
@@ -3,4 +3,4 @@ MONGODB_HOST = '127.0.0.1'
 MONGODB_PORT = 27017
 
 DB = 'zhibiao'
-collection = 'infos_one'
+COLLECTION = 'infos_one'
